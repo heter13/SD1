ee6f199d8fe46138377e414166d3c7e73824c008
heter13 <hespinoza@ucsd.edu>
2022-02-03 21:58:36 -0800

    SD1

--- SD1.java
+++ SD1.java
@@ -1,2 +1,7 @@
 public class SD1 {
+
+    public static int multiplication(int x, int y) {
+        return x*y;
+    }
+    
 }
---
+++ SD1Test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SD1Test {
+
+    @Test
+    public void test1() {
+        assertEquals(5, SD1.multiplication(2, 3));
+    }
+    
+}
